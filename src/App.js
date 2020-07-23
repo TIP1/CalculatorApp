4e060875d4dd8d6aebfceb815a28b7f7a019f431
TIP1 <tip219@bk.ru>
2020-07-23 11:12:23 +0300

    dif resolve

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="caclulator">
+        <div className="lineofnum">
+          <form name="form">
+            <input name="numbers" type="text" readOnly/>
+          </form>
+        </div>
+        <div className="clean">C</div>
+        <div className="del">&larr;</div>
+        <div className="plus">+</div>
+        <div className="minus">-</div>
+        <div className="mul">&times;</div>
+        <div className="divi">&divide;</div>
+        <div className="number">7</div>
+        <div className="number">8</div>
+        <div className="number">9</div>
+        <div className="item">(</div>
+        <div className="number">4</div>
+        <div className="number">5</div>
+        <div className="number">6</div>
+        <div className="item">)</div>
+        <div className="number">1</div>
+        <div className="number">2</div>
+        <div className="number">3</div>
+        <div className="eq">=</div>
+        <div className="zero">0</div>
+        <div className="dot">.</div>
+      </div>
     </div>
   );
 }
